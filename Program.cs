9154a920a94a57c1bdcb7a6125da43d9ea4bc8f4
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-02-28 13:27:51 -0600

    method practice

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+
+        static void Main (string[] args)
+        {
+
+            List<string> planetList = new List<string> () { "Mercury", "Mars", };
+
+            planetList.Add ("Jupiter");
+            planetList.Add ("Saturn");
+
+            List<string> lastPlanetList = new List<string> () { "Uranus", "Neptune", };
+
+            planetList.AddRange (lastPlanetList);
+
+            planetList.Insert (3, "Earth");
+            planetList.Insert (4, "Venus");
+
+            planetList.Add ("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange (0, 4);
+
+            planetList.Remove ("Pluto");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine (planet);
+            }
+
+        }
+    }
+}
